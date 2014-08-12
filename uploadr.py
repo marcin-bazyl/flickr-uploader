2266e08f79abf8796b2d83e92aeb65112cf2688d
administrator <admin@qnap119.(none)>
2014-08-12 22:23:59 +0100

    changed base URI to https

--- uploadr.py
+++ uploadr.py
@@ -141,7 +141,7 @@ class APIConstants:
     """ APIConstants class
     """
 
-    base = "http://api.flickr.com/services/"
+    base = "https://api.flickr.com/services/"
     rest   = base + "rest/"
     auth   = base + "auth/"
     upload = base + "upload/"
